6e4a0efb84f5577eafa74bd33099cb93fe806e35
Federico Yulita <federico_yulita@hotmail.com>
2020-08-24 23:00:36 -0300

    Funciona Heroku! Agrego redireccion automatica a HTTPS y pongo DEBUG en False

--- fisuba/fisuba/settings.py
+++ fisuba/fisuba/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@p6us%6c!4-pxte98-ui&rqwdhsvm91ujq2wnl2z*wc+x+*c7%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['fisuba.herokuapp.com', '127.0.0.1']
 
@@ -161,7 +161,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Always open as HTTPS // False when in development and True when in production
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
